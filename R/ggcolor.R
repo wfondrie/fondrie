9374ae34161316a8a3fba389dec908e77f1c2efa
wfondrie <wfondrie@umaryland.edu>
2016-07-12 16:25:55 -0400

    added ggcolor()

---
+++ R/ggcolor.R
@@ -0,0 +1,17 @@
+#' Emulate ggplot2 colors
+#'
+#' @description
+#' Creates a vector of n colors spread evenly across hues, thereby emulating
+#' the default color scheme of ggplot2.
+#'
+#' @param n Number of colors
+#' @return A vector of length n containing hexidecimal color codes.
+#' @examples
+#' ggcolor(2)
+#' @references
+#' http://stackoverflow.com/questions/8197559/emulate-ggplot2-default-color-palette
+
+ggcolor <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
